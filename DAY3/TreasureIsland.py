95c7e72589c0084cd51a9e4378dbfa4b7a2b8470
Shirin Wafiqah <shirinwafiqah2@gmail.com>
2026-08-05 21:43:28 +0530

    Modified if/else statements

--- DAY3/TreasureIsland.py
+++ DAY3/TreasureIsland.py
@@ -17,12 +17,12 @@ print("Y0u are at a cross road. Where do you want to go? Type 'left' or 'right'"
 dir = input().lower()
 if dir == "right":
     print("You fell into a hole. Game Over.")
-elif dir == "left":
+else:
     print("You come to a lake. There is an island in the middle of the lake. Type 'wait' or 'swim' to cross the lake.")
     ip = input().lower()
     if ip == "swim":
         print("Attacked by trout. Game Over.")
-    elif ip == "wait":
+    else:
         print("You arrive at the island unharmed. There is a house with 3 doors. One red, one yellow and one blue. Which colour do you choose?")
         door = input().lower()
         if door == "red":
