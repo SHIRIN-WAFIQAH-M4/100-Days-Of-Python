7bb5c019fe9c3c1cb34f8aea53b64b142845c3ee
Shirin Wafiqah <shirinwafiqah2@gmail.com>
2026-08-10 19:34:56 +0530

    Day 7: Hangman Game

---
+++ DAY 7/Hangman.py
@@ -0,0 +1,113 @@
+import random
+
+word_list = ["python", "java", "kotlin", "javascript"]
+chosen_word = random.choice(word_list)
+
+print("Welcome to Hangman!")
+print("The word has", len(chosen_word), "letters.")
+
+placeholder = "_" * len(chosen_word)
+print(placeholder)
+
+lives = 6
+
+stages = [
+    """
+       -----
+       |   |
+           |
+           |
+           |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+           |
+           |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+       |   |
+           |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+      /|   |
+           |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+           |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+      /    |
+           |
+    =========
+    """,
+
+    """
+       -----
+       |   |
+       O   |
+      /|\\  |
+      / \\  |
+           |
+    =========
+    """
+]
+
+while "_" in placeholder and lives > 0:
+
+    guess = input("Guess a letter: ").lower()
+
+    display = ""
+
+    for l in range(len(chosen_word)):
+        if chosen_word[l] == guess:
+            display += guess
+        else:
+            display += placeholder[l]
+
+    if guess not in chosen_word:
+        lives -= 1
+        print("Wrong guess!")
+        print("Lives remaining:", lives)
+
+    placeholder = display
+
+    print(placeholder)
+    print(stages[6 - lives])
+
+if "_" not in placeholder:
+    print("🎉 You won!")
+    print("The word was:", chosen_word)
+else:
+    print("💀 You lost!")
+    print("The word was:", chosen_word)
